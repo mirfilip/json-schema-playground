0555ee8a85b884a1df1113ab1650846841d4154a
Mirosław Filip <miroslaw.filip@schibsted.pl>
2016-04-12 00:40:14 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$inputData = json_decode($argv[1]);
+
+$retriever = new \JsonSchema\Uri\UriRetriever;
+$jsonSchemaPath = __DIR__ . '/schema/';
+
+$schema = $retriever->retrieve('file://' . $jsonSchemaPath . '203.json');
+
+$resolver = new \JsonSchema\RefResolver($retriever);
+$resolver->resolve($schema, 'file://' . $jsonSchemaPath);
+
+$validator = new \JsonSchema\Validator();
+$validator->check($inputData, $schema);
+
+echo 'Input '. json_encode($inputData) . PHP_EOL;
+$status = $validator->isValid();
+echo 'Validation status (Validator::isValid): '; var_export($status); echo PHP_EOL;
+if (!$validator->isValid()) {
+    echo 'Errors: ' . PHP_EOL;
+    echo json_encode($validator->getErrors(), JSON_PRETTY_PRINT);
+    echo PHP_EOL;
+}
